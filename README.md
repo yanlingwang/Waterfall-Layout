b82e2985b44a6e2de9692a5e1d5bde095e11405c
yanling <yanlingw95@gmail.com>
2016-09-09 10:52:28 +0800

    add

---
+++ README.md
@@ -0,0 +1,4 @@
+
+<p>javascript实现瀑布流布局</p>
+<p>预览地址</p>
+<address>http://htmlpreview.github.io/?https://github.com/yanlingwang/test/blob/master/waterfall.html</address>
---
+++ script.js
@@ -0,0 +1,75 @@
+window.onload=function(){
+	waterfall('main','box');
+	var dataInt={"data":[{"src":'0.jpg'},{"src":'1.jpg'},{"src":'2.jpg'}]}
+	window.onscroll=function(){
+		if(checkScrollSlide){
+			var oParent=document.getElementById('main');
+            //将数据块渲染到页面尾部
+           for(var i=0;i<dataInt.data.length;i++){
+               var oBox=document.createElement('div');
+               oBox.className='box';
+               oParent.appendChild(oBox);
+               var oPic=document.createElement('div');
+               oPic.className='pic';
+               oBox.appendChild(oPic);
+               var oImg=document.createElement('img');
+               oImg.src="images/"+dataInt.data[i].src;
+               oPic.appendChild(oImg);
+           }
+           waterfall('main','box');
+		}
+		
+	}
+}
+function waterfall(parent,box){
+	//将main下的所有class为box的元素取出来
+	var oParent=document.getElementById(parent);
+	var oBoxs=getByClass(oParent,box);
+	//计算整个页面显示的列数
+	var oBoxW=oBoxs[0].offsetWidth;//每个盒子的宽度,这里为200+20+2+15=237
+    var cols=Math.floor(document.documentElement.clientWidth/oBoxW);//获取整个页面的宽度在获得列数
+    //设置main的宽度
+    oParent.style.cssText='width:'+oBoxW*cols+'px;margin:0 auto';
+    var hArr=[];//存放每一列高度的数组
+    for(var i=0;i<oBoxs.length;i++){
+    	if(i<cols){
+    		hArr.push(oBoxs[i].offsetHeight);
+    	}else{
+    		var minH=Math.min.apply(null,hArr);
+    		var index=getMinhIndex(hArr,minH);
+    		oBoxs[i].style.position='absolute';
+    		oBoxs[i].style.top=minH+'px';
+    		oBoxs[i].style.left=oBoxW*index+'px';
+    		hArr[index]+=oBoxs[i].offsetHeight;
+    	}
+    }
+    console.log(hArr);
+}
+//根据class获取元素
+function getByClass(parent,clsName){
+	var boxArr=new Array(),//用来存储获取到的所有class为box的元素
+	    oElements=parent.getElementsByTagName("*");
+	for(var i=0;i<oElements.length;i++){
+		if(oElements[i].className==clsName){
+			boxArr.push(oElements[i]);
+		}
+	}
+	return boxArr;
+}
+function getMinhIndex(arr,val){
+	for(var i in arr){
+        if(arr[i]==val){
+        	return i;
+        }
+	}
+}
+//检测是否具备加载数据块的条件
+function checkScrollSlide(){
+	var oParent=document.getElementById("main");
+	var oBoxs=getByClass(oParent,'box');
+	var lastBoxH=oBoxs[oBoxs.length-1].offsetTop+
+	             Math.floor(oBoxs[oBoxs.length-1].offsetHeight/2);
+    var scrollTop=document.body.scrollTop||document.documentElement.scrollTop;
+    var height=document.body.clientHeight||document.documentElement.clientHeight;
+    return (lastBoxH<scrollTop+height)?true:false; 
+}
